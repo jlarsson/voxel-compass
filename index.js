a6db9410913bc10a402ef41ed2692600d259ff4c
jlarsson <mimmertim@gmail.com>
2014-08-22 22:18:22 +0200

    Formatting

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
+/*jslint node:true*/
 (function (module) {
     "use strict"
-    var extend = require('extend')
-    var inherits = require('inherits')
-    var EventEmitter = require('events').EventEmitter
+    var extend = require('extend'),
+        inherits = require('inherits'),
+        EventEmitter = require('events').EventEmitter
 
-        function Compass(game, opts) {
-            this.game = game
-            if (!game.isClient) {
-                return
-            }
-
-            var options = extend({}, opts || {}, {
-                threshold: 1,
-                north: [0, 0, 1]
-            })
-
-
-            var angle = function (v) {
-                return Math.atan2(v[0], v[2]) * 180 / Math.PI
-            }
-
-            var gameItem = {
-                game: game,
-                compass: this,
-                lastAngle: 360000,
-                threshold: options.threshold,
-                angle: angle,
-                northAngle: angle(options.north),
-                
-                tick: function () {
-                    var cv = this.game.cameraVector()
-                    var angle = (this.angle(cv) - this.northAngle) % 360
-                    
-                    if (Math.abs(angle - this.lastAngle) > this.threshold){
-                        this.lastAngle = angle
-                        this.compass.emit('compass-direction', angle)
-                    }
-
-                }
-            }
-
-            game.addItem(gameItem)
+    function Compass(game, opts) {
+        this.game = game
+        if (!game.isClient) {
+            return
         }
+
+        var options = extend({}, opts || {}, {
+            threshold: 1,
+            north: [0, 0, 1]
+        })
+
+
+        var angle = function (v) {
+            return Math.atan2(v[0], v[2]) * 180 / Math.PI
+        }
+
+        var gameItem = {
+            game: game,
+            compass: this,
+            lastAngle: 360000,
+            threshold: options.threshold,
+            angle: angle,
+            northAngle: angle(options.north),
+
+            tick: function () {
+                var cv = this.game.cameraVector()
+                var angle = (this.angle(cv) - this.northAngle) % 360
+
+                if (Math.abs(angle - this.lastAngle) > this.threshold) {
+                    this.lastAngle = angle
+                    this.compass.emit('compass-direction', angle)
+                }
+
+            }
+        }
+
+        game.addItem(gameItem)
+    }
 
     inherits(Compass, EventEmitter)
 
