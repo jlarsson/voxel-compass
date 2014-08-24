9e48230fd59106c4fe9e7d43bc6c36d94a08191d
Joakim Larsson <mimmertim@gmail.com>
2014-08-24 13:57:09 +0200

    support for dom element in options
    The DOM element will be rotated to reflect compass direction

--- index.js
+++ index.js
@@ -5,44 +5,67 @@
         inherits = require('inherits'),
         EventEmitter = require('events').EventEmitter
 
-    function Compass(game, opts) {
-        this.game = game
-        if (!game.isClient) {
-            return
-        }
+        function Compass(game, opts) {
+            this.game = game
+            if (!game.isClient) {
+                return
+            }
 
-        var options = extend({}, opts || {}, {
-            threshold: 1,
-            north: [0, 0, 1]
-        })
+            var options = extend({}, opts || {}, {
+                threshold: 1,
+                north: [0, 0, 1]
+            })
 
 
-        var angle = function (v) {
-            return Math.atan2(v[0], v[2]) * 180 / Math.PI
-        }
+            var angle = function (v) {
+                return Math.atan2(v[0], v[2]) * 180 / Math.PI
+            }
 
-        var gameItem = {
-            game: game,
-            compass: this,
-            lastAngle: 360000,
-            threshold: options.threshold,
-            angle: angle,
-            northAngle: angle(options.north),
+            var gameItem = {
+                game: game,
+                compass: this,
+                lastAngle: 360000,
+                threshold: options.threshold,
+                angle: angle,
+                northAngle: angle(options.north),
 
-            tick: function () {
-                var cv = this.game.cameraVector()
-                var angle = (this.angle(cv) - this.northAngle) % 360
+                tick: function () {
+                    var cv = this.game.cameraVector()
+                    var angle = (this.angle(cv) - this.northAngle) % 360
 
-                if (Math.abs(angle - this.lastAngle) > this.threshold) {
-                    this.lastAngle = angle
-                    this.compass.emit('compass-direction', angle)
+                    if (Math.abs(angle - this.lastAngle) > this.threshold) {
+                        this.lastAngle = angle
+                        this.compass.emit('compass-direction', angle)
+                    }
+
                 }
+            }
 
+            game.addItem(gameItem)
+
+
+            function getAutoElement(element) {
+                element = (typeof element) === 'string' ? document.getElementById(element) : element;
+                if (!element) {
+                    return;
+                }
+                var transformCandidates = ['transformProperty', 'webkitTransform', 'oTransform', 'msTransform', 'mozTransform'];
+                for (var i = 0; i < transformCandidates.length; ++i) {
+                    if (element.style[transformCandidates[i]] !== undefined) {
+                        return {
+                            element: element,
+                            transform: transformCandidates[i]
+                        }
+                    }
+                }
+            }
+            var auto = getAutoElement(opts.element);
+            if (auto) {
+                this.on('compass-direction', function (direction) {
+                    auto.element.style[auto.transform] = 'rotate(' + direction + 'deg)';
+                });
             }
         }
-
-        game.addItem(gameItem)
-    }
 
     inherits(Compass, EventEmitter)
 
